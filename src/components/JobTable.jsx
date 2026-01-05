2bf3198e8d8f870e24d82e87aee1dc2e2808019a
unknown <aryasajith504@gmail.com>
2026-01-05 19:04:27 +0530

    first commit

--- src/components/JobTable.jsx
+++ src/components/JobTable.jsx
@@ -3,26 +3,29 @@ import JobRow from "./JobRow";
 function JobTable({ jobs, deleteJob, openEditModal }) {
     return (
         <section className="job-list">
-            <table style={{ border: "solid, black, 1px" }}>
-                <thead>
-                    <tr>
-                        <th>Company</th>
-                        <th>Role</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {jobs.map(job => (
-                        <JobRow
-                            key={job.id}
-                            job={job}
-                            deleteJob={deleteJob}
-                            openEditModal={openEditModal}
-                        />
-                    ))}
-                </tbody>
-            </table>
+            {/* Wrapper for mobile scroll */}
+            <div className="table-wrapper">
+                <table style={{ border: "solid, black, 1px" }}>
+                    <thead>
+                        <tr>
+                            <th>Company</th>
+                            <th>Role</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {jobs.map(job => (
+                            <JobRow
+                                key={job.id}
+                                job={job}
+                                deleteJob={deleteJob}
+                                openEditModal={openEditModal}
+                            />
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </section>
     );
 }
